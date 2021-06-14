81cb1cf2bb8b0f420289054111c621c4efae3893
John Meinken <thebellsofohio@hotmail.com>
2021-06-14 17:39:23 -0400

    got docker working with sqlite3

--- dockerfiles/chiron_rdcrn/entrypoint.sh
+++ dockerfiles/chiron_rdcrn/entrypoint.sh
@@ -3,10 +3,11 @@
 rm db.sqlite3
 rm staging.sqlite3
 
-python manage.py makemigrations usermanager
-python manage.py migrate
-python manage.py redcap_restore_dd
-python manage.py chiron_restore_dd
-python manage.py collectstatic --no-input --settings=project.settings_docker
+python dockermanage.py makemigrations usermanager
+python dockermanage.py migrate
+python dockermanage.py migrate --database=staging
+python dockermanage.py redcap_restore_dd
+python dockermanage.py chiron_restore_dd
+python dockermanage.py collectstatic --no-input
 
 gunicorn project.wsgi_docker:application --bind 0.0.0.0:8000
